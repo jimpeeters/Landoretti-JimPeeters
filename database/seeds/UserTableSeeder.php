539c9e1064a956cd3034e2dbf44cbc083e9a8a2f
Jim Peeters <Jim Peeters>
2015-12-30 18:52:26 +0100

    Back-end : betere auction seed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -35,6 +35,16 @@ class UserTableSeeder extends Seeder
 				'number' => '0479334587',
 				'address' => 'Janlaan 12'
 			),
+		array(
+				'name' => 'Jos Joskens',
+				'email' => 'jos@test.be',
+				'password' => Hash::make('root'),
+				'FK_country_id' => '2',
+				'zipcode' => '2662',
+				'city' => 'josland',
+				'number' => '0476334587',
+				'address' => 'Joslaan 12'
+			),
 
 		);
 
